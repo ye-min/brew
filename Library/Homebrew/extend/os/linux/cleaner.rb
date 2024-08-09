c4f9e5eccd8a87dc2a7d58bcd50da25f5c511369
Douglas Eichelberger <dduugg@gmail.com>
2024-08-09 12:44:03 -0700

    PoC: Use a factory rather than dynamically redefine methods

--- Library/Homebrew/extend/os/linux/cleaner.rb
+++ Library/Homebrew/extend/os/linux/cleaner.rb
@@ -1,10 +1,10 @@
 # typed: strict
 # frozen_string_literal: true
 
-class Cleaner
+class CleanerLinux < Cleaner
   private
 
-  sig { params(path: Pathname).returns(T::Boolean) }
+  sig { override.params(path: Pathname).returns(T::Boolean) }
   def executable_path?(path)
     path.elf? || path.text_executable?
   end
--- Library/Homebrew/extend/os/mac/cleaner.rb
+++ Library/Homebrew/extend/os/mac/cleaner.rb
@@ -1,12 +1,10 @@
 # typed: strict
 # frozen_string_literal: true
 
-class Cleaner
+class CleanerMac < Cleaner
   private
 
-  undef executable_path?
-
-  sig { params(path: Pathname).returns(T::Boolean) }
+  sig { override.params(path: Pathname).returns(T::Boolean) }
   def executable_path?(path)
     path.mach_o_executable? || path.text_executable?
   end
